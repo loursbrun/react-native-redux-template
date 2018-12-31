0ed9df315e6d691b58e2c34dd410c940a470d2f4
fbrun <fabien.brun@kcitylabs.fr>
2018-12-31 11:22:41 +0100

    add redux for all views

--- screens/Screen2.js
+++ screens/Screen2.js
@@ -2,22 +2,46 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
+// Redux import
+import { connect } from 'react-redux';
+import {
+  testAction,
+} from './redux';
+import { Provider } from 'react-redux';
+import { store } from './redux';
+
+
+class AppInner extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Screen2 </Text>
+        <Button title="Test" onPress={this.props.testAction}></Button>
+          <Text> {this.props.content}</Text>
+      </View>
+    );
+  }
+}
+
+
+// Redux Provider
 export default class Screen2 extends React.Component {
   static navigationOptions = {
     header: null,
   };
-
   render() {
     return (
-      <View style={styles.container}>
-          <Text> Screen2 </Text>
-      </View>
-      );
-    }
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
   }
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -26,3 +50,23 @@ const styles = StyleSheet.create({
     marginTop: 50
   }
 });
+
+
+// Redux Actions & Connect
+const mapStateToProps = (state) => ({
+  content: state.dvas0004.content || "Please Wait...",
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    testAction: () => {
+      dispatch(testAction())
+    }
+  }
+};
+
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppInner);
+
