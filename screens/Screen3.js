a0e14eca2c6fb5c184f0c4e8c07c49df6e19c37e
fbrun <fabien.brun@kcitylabs.fr>
2018-12-31 11:49:04 +0100

    remove redux

--- screens/Screen3.js
+++ screens/Screen3.js
@@ -2,46 +2,23 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View,
-  Button
+  View
 } from 'react-native';
 
-// Redux import
-import { connect } from 'react-redux';
-import {
-  testAction,
-} from './redux';
-import { Provider } from 'react-redux';
-import { store } from './redux';
 
-
-class AppInner extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text> Screen3 </Text>
-        <Button title="Test" onPress={this.props.testAction}></Button>
-          <Text> {this.props.content}</Text>
-      </View>
-    );
-  }
-}
-
-
-// Redux Provider
 export default class Screen3 extends React.Component {
   static navigationOptions = {
     header: null,
   };
+
   render() {
     return (
-      <Provider store={store}>
-        <AppContainer />
-      </Provider>
-    );
+      <View style={styles.container}>
+          <Text> Screen1 </Text>
+      </View>
+      );
+    }
   }
-}
-
 
 const styles = StyleSheet.create({
   container: {
@@ -50,23 +27,3 @@ const styles = StyleSheet.create({
     marginTop: 50
   }
 });
-
-
-// Redux Actions & Connect
-const mapStateToProps = (state) => ({
-  content: state.dvas0004.content || "Please Wait...",
-});
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    testAction: () => {
-      dispatch(testAction())
-    }
-  }
-};
-
-const AppContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AppInner);
-
