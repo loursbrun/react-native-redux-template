130def5ec76207341e53135f825b79044e4c634e
fbrun <fabien.brun@kcitylabs.fr>
2019-01-02 15:34:26 +0100

    fix last key increment object

--- redux/actions/todo.js
+++ redux/actions/todo.js
@@ -1,4 +1,7 @@
 export const addTodo = (todo) => {
+    console.log("new index : " + todo.length.toString())
+    console.log("last index : " + todo[todo.length - 1].key)
+    console.log("last index : " + (parseInt(todo[todo.length - 1].key)+1).toString())
     return {
         type: 'ADDTODO',
         todo: [...todo,
